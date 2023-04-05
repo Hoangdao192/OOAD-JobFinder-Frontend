d03f52cebdbd37af7d09f66788a5b03e1e56f052
pvtdung0612 <pvtdung0612@gmail.com>
2023-04-05 20:15:02 +0700

    change endlish to vietnamese

--- src/pages/candidates/CandidateHome.jsx
+++ src/pages/candidates/CandidateHome.jsx
@@ -140,19 +140,19 @@ export const CandidateHome = () => {
             <div className="space-y-3 w-6/12">
                <div className="bg-cover opacity-90 rounded-xl w-full h-xl bg-[url('./image/candidates/BackgroundSearch.png')]">
                   <div className="space-y-3 p-5 pb-2 text-gray-100">
-                     <div className=" text-xl">Are you looking for a dream job?</div>
+                     <div className=" text-xl">Bạn đang tìm kiếm một công việc mơ ước?</div>
 
-                     <p className="text-xs">Job Finder is place where you can find your dream job in various skills, more than 10,000 jobs are available here</p>
+                     <p className="text-xs">Job Finder là nơi bạn có thể tìm thấy công việc mơ ước của mình với nhiều kỹ năng khác nhau, hơn 10.000 việc làm có sẵn tại đây</p>
 
                      <form onSubmit={handleSubmitSearch} className="flex items-center space-x-3">
                         <div className="relative w-full">
                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                               <svg aria-hidden="true" className="w-4 h-4 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
                            </div>
-                           <input type="text" id="ipt_search" className="p-3 text-xs bg-gray-100 bg-opacity-20 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-1 text-white rounded-md block w-full pl-10 placeholder-white" placeholder="Search" />
+                           <input type="text" id="ipt_search" className="p-3 text-xs bg-gray-100 bg-opacity-20 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-1 text-white rounded-md block w-full pl-10 placeholder-white" placeholder="Tên công việc" />
                         </div>
-                        <button type="submit" className="p-2 w-auto h-10 text-sm text-common_color rounded-md bg-white hover:bg-hover_common_color hover:text-white">
-                           <span className="m-3">Search</span>
+                        <button type="submit" className="w-auto whitespace-nowrap h-10 text-sm text-common_color rounded-md bg-white hover:bg-hover_common_color hover:text-white">
+                           <span className="m-3">Tìm kiếm</span>
                         </button>
                      </form>
                      <div>
